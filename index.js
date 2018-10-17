0d713fb94f2c5613052e3f6574466454bbc9b2df
Samantha Hyatt <shhyatt@gmail.com>
2018-10-17 19:51:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var greeting = "";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
 var conversation = `${topic}`;
+"Let's talk about space";
+
